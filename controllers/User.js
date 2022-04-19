eb3faa6035662b3087b31daf35f132d6b3a2984b
imronshoimov <imronshoimov125@gmail.com>
2022-04-19 14:59:38 +0500

    creating error handling

--- controllers/User.js
+++ controllers/User.js
@@ -1,16 +1,28 @@
 const UserModel = require("../models/User");
+const code = require("../config/status");
 
 module.exports = class {
   async insert(req, res) {
     try {
+      const key = ["firstName", "lastName", "age", "gender"];
+
+      for (let item of key) {
+        if (!req.body[item]) {
+          res.status(400).json({
+            code: code.BAD_REQUEST,
+            message: `${item} is required`,
+          });
+        }
+      }
+
       const newUser = await UserModel.create(req.body);
 
-      res.status(201).send({
-        message: "OK",
-        data: newUser,
-      });
+      res.status(201).send(newUser);
     } catch (err) {
-      throw new Error(`User controller error: ${e}`);
+      return {
+        code: code.INTERNAL,
+        message: err.message,
+      };
     }
   }
 };
