58252c1bde806ab5bc1330bbe61eb906ee11919e
imronshoimov <imronshoimov125@gmail.com>
2022-04-19 12:02:34 +0500

    user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: String,
+});
+
+module.exports = mongoose.model("User", UserSchema);
