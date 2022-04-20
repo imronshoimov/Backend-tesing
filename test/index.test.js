0ccf36db21e94c630f9ebe129a5d852b901d3aef
imronshoimov <imronshoimov125@gmail.com>
2022-04-20 15:11:57 +0500

    updating tests and force shutdown of server

--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,16 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+require("dotenv").config();
 const server = require("../test");
-const expect = chai.expect;
 
-chai.should();
-
-chai.use(chaiHttp);
-
-const newUser = {
-  firstName: "Imron",
-  lastName: "Shoimov",
-  age: 18,
-  gender: "male",
-};
-
-describe("User", () => {
-  describe("Create User", () => {
-    it("should create a new user", () => {
-      chai
-        .request(server)
-        .post("/user")
-        .send(newUser)
-        .end((err, response) => {
-          expect(err).to.equal(null);
-          expect(typeof response).to.equal("object");
-        });
-    });
+function shutDown() {
+  server.close(() => {
+    process.exit(0);
   });
+
+  setTimeout(() => {
+    process.exit(1);
+  }, 10000);
+}
+
+after(async () => {
+  await shutDown();
 });
