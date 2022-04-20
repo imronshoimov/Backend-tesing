fa6491183aadbbf6a072482cba058f0520e8a086
imronshoimov <imronshoimov125@gmail.com>
2022-04-20 16:08:06 +0500

    test creating user

--- test/user.test.js
+++ test/user.test.js
@@ -7,7 +7,7 @@ chai.should();
 
 chai.use(chaiHttp);
 
-const newUser = {
+const exampleUser = {
   firstName: "Imron",
   lastName: "Shoimov",
   age: 18,
@@ -17,13 +17,53 @@ const newUser = {
 describe("User", () => {
   describe("Create User", () => {
     it("should create a new user", () => {
+      const example = { ...exampleUser };
       chai
         .request(server)
         .post("/user")
-        .send(newUser)
+        .send(example)
         .end((err, response) => {
           expect(err).to.equal(null);
           expect(typeof response).to.equal("object");
+        });
+    });
+
+    it("should return required if firstname does not exists", () => {
+      const example = { ...exampleUser };
+      delete example.firstName;
+      chai
+        .request(server)
+        .post("/user")
+        .send(example)
+        .end((err, response) => {
+          expect(response.body.message).includes("firstName is required");
+          expect(err).to.equal(null);
+        });
+    });
+
+    it("should return required if lastname does not exists", () => {
+      const example = { ...exampleUser };
+      delete example.lastName;
+      chai
+        .request(server)
+        .post("/user")
+        .send(example)
+        .end((err, response) => {
+          expect(response.body.message).includes("lastName is required");
+          expect(err).to.equal(null);
+        });
+    });
+
+    it("should return required if age does not exists", () => {
+      const example = { ...exampleUser };
+      delete example.age;
+      chai
+        .request(server)
+        .post("/user")
+        .send(example)
+        .end((err, response) => {
+          expect(response.body.message).includes("age is required");
+          expect(err).to.equal(null);
         });
     });
   });
